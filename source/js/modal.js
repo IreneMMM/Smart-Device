17efec19b085ac0901446aa72e6edf672f9a4604
IreneMMM <menshovaiv@gmail.com>
2021-12-06 09:42:23 +0300

    Исправления JS

--- source/js/modal.js
+++ source/js/modal.js
@@ -73,47 +73,4 @@
       closemodal();
     }
   });
-
-  // скролл
-
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-
-  // аккордеон
-
-  var accordionButton = document.querySelector('.site-navigation__toggle');
-  var contactsButton = document.querySelector('.contacts__toggle');
-  var navigation = document.querySelector('.page-footer__navigation');
-  var contacts = document.querySelector('.page-footer__contacts');
-
-  navigation.classList.add('page-footer__navigation--closed');
-  contacts.classList.add('page-footer__contacts--closed');
-
-  accordionButton.addEventListener('click', function () {
-    if (navigation.classList.contains('page-footer__navigation--closed')) {
-      navigation.classList.remove('page-footer__navigation--closed');
-      if (!contacts.classList.contains('page-footer__contacts--closed')) {
-        contacts.classList.add('page-footer__contacts--closed');
-      }
-    } else {
-      navigation.classList.add('page-footer__navigation--closed');
-    }
-  });
-
-  contactsButton.addEventListener('click', function () {
-    if (contacts.classList.contains('page-footer__contacts--closed')) {
-      contacts.classList.remove('page-footer__contacts--closed');
-      if (!navigation.classList.contains('page-footer__navigation--closed')) {
-        navigation.classList.add('page-footer__navigation--closed');
-      }
-    } else {
-      contacts.classList.add('page-footer__contacts--closed');
-    }
-  });
 })();
